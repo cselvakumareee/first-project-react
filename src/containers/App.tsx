6b003d4c04619b86a83651052cdf340b7c0db748
cselvakumareee <48525562+cselvakumareee@users.noreply.github.com>
2020-03-20 00:39:32 +0530

    Routing with switch

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -2,64 +2,71 @@ import React, { Component } from 'react';
 import './App.scss';
 import Persons from '../components/persons/Persons';
 import Cockpit from '../components/cockpit/cockpit';
+import Tabs from '../components/Tabs';
 
-class App extends Component{
-  constructor(props:any){
+import Home from '../components/home/Home';
+import User from '../components/user/User';
+import Contact from '../components/contact/Contact';
+
+import { Switch, Route, Link, BrowserRouter as Router } from 'react-router-dom';
+
+class App extends React.Component {
+  constructor(props: any) {
     super(props);
     console.log('[App.tsx] constructor');
   }
-  state={
-    className:[
-      { id:'1', name:"selva", age: 40},
-      { id:'2', name:"raja", age: 80},
-      { id:'3', name:"shiva", age: 60}
+  state = {
+    className: [
+      { id: '1', name: 'selva', age: 40 },
+      { id: '2', name: 'raja', age: 80 },
+      { id: '3', name: 'shiva', age: 60 }
     ],
     showPerson: false,
     showCockpit: true
   };
 
-  static getDerivedStateFromProps(props:any, state:any){
+  static getDerivedStateFromProps(props: any, state: any) {
     console.log('[App.tsx] getDerivedStateFromProps', props);
     return state;
   }
 
-  componentDidMount(){
+  componentDidMount() {
     console.log('[App.tsx] componentdidmount');
   }
 
-  shouldComponentUpdate(nextProps:any, nextState:any){
+  shouldComponentUpdate(nextProps: any, nextState: any) {
     console.log('[App.tsx] shouldcomponentupdate');
     return true;
   }
 
-  componentDidUpdate(){
+  componentDidUpdate() {
     console.log('[App.tsx] componentdidupdate');
   }
 
-  clickEventHandler = (newName:any) =>
-  this.setState({
-    className:[
-      { name:"selvakumar c", age: 40},
-      { name:"raja dhaya", age: 80},
-      { name:"shiva j", age: 60}
-    ]
-  })
+  clickEventHandler = (newName: any) =>
+    this.setState({
+      className: [
+        { name: 'selvakumar c', age: 40 },
+        { name: 'raja dhaya', age: 80 },
+        { name: 'shiva j', age: 60 }
+      ]
+    });
 
-  nameChangeHandler = (eventName:any, id:any) => {
-  const personIndex = this.state.className.findIndex(p =>{
-    return p.id === id;
-  });
+  nameChangeHandler = (eventName: any, id: any) => {
+    const personIndex = this.state.className.findIndex(p => {
+      return p.id === id;
+    });
 
-  const person = {
-    ...this.state.className[personIndex]
+    const person = {
+      ...this.state.className[personIndex]
+    };
+    person.name = eventName.target.value;
+
+    const className = [...this.state.className];
+
+    className[personIndex] = person;
+    this.setState({ className: className });
   };
-  person.name = eventName.target.value;
-
-  const className = [...this.state.className];
-  
-  className[personIndex] = person;
-  this.setState({className:className});
-  }
   // this.setState({
   //   className:[
   //   { name:"selvakumar c", age: 40},
@@ -70,49 +77,96 @@ class App extends Component{
 
   toggleEventHandler = () => {
     const doesShow = this.state.showPerson;
-    this.setState({showPerson:!doesShow});
-  }
+    this.setState({ showPerson: !doesShow });
+  };
 
-  deleteEventHandler = (personDelIndex:any) =>{
+  deleteEventHandler = (personDelIndex: any) => {
     const personDel = this.state.className;
-    personDel.splice(personDelIndex,1);
-    this.setState({personDel:personDel});
-  }
+    personDel.splice(personDelIndex, 1);
+    this.setState({ personDel: personDel });
+  };
 
-  render(){
-    
+
+  render() {
     console.log('[App.tsx] render');
 
+    
+  
+
     const appStyle = {
-      backgroundColor: 'pink',
+      backgroundColor: 'pink'
     };
 
     let personName = null;
-    if(this.state.showPerson){
+    if (this.state.showPerson) {
       personName = (
-         <div>
-           <Persons className = {this.state.className}
-           clicked = {this.deleteEventHandler}
-           changed = {this.nameChangeHandler}/>
-         </div>
+        <div>
+          <Persons className={this.state.className} clicked={this.deleteEventHandler} changed={this.nameChangeHandler} />
+        </div>
       );
     }
-    return(
-      
-      
-      <div className = "App">
-        <button onClick={()=>{
-      this.setState({showCockpit:false});
-    }}>Remove cockpit</button>
-        {this.state.showCockpit ?<Cockpit
-         clicked = {this.toggleEventHandler}/>:null}
-          {personName}
-        
+    return (
+      <div className="App">
+        <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/contact">contact</Link>
+            </li>
+            <li>
+              <Link to="/user">User</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/user">
+            <User />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
       </div>
-    )
+    </Router>
+   
+        <button
+          onClick={() => {
+            this.setState({ showCockpit: false });
+          }}
+        >
+          Remove cockpit
+        </button>
+        {this.state.showCockpit ? <Cockpit clicked={this.toggleEventHandler} /> : null}
+        {personName}
+        {/* Tab-functionality, its working
+        <div>
+          <h1>Tabs Demo</h1>
+          <main>
+            <Tabs>
+              <a href="https://www.youtube.com/results?search_query=how+to+make+tabs+in+react+js" target="_blank">
+                One
+              </a>
+              <span>Well here's a nested Tabs element.</span>
+              Two
+              <span>Two thing</span>
+              Three
+              <span>Three thing</span>
+            </Tabs>
+          </main>
+        </div>*/}
+      </div> 
+    );
   }
 }
-
-
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,33 @@ import './index.scss';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Home from './components/home/Home';
+import User from './components/user/User';
+import Contact from './components/contact/Contact';
+
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
+// const routing = (
+// <Router>
+// <div>
+// <ul>
+//   <li>
+//     <Link to="/">Home</Link>
+//   </li>
+//   <li>
+//     <Link to="/user">Users</Link>
+//   </li>
+//   <li>
+//     <Link to="/contact">Contact</Link>
+//   </li>
+// </ul>
+//   <Route exact path="/" component={Home} />
+//   <Route path="/user" component={User} />
+//   <Route path="/contact" component={Contact} />
+// </div>
+// </Router>
+// )
+
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
