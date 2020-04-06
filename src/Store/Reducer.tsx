8b5d107b784a779ae40ea0fd7fcdccc1d874e54d
selva <cselvakumareee@gmail.com>
2020-04-07 01:12:55 +0530

    bug

--- src/Store/Reducer.tsx
+++ src/Store/Reducer.tsx
@@ -3,6 +3,7 @@ import Counter from '../containers/Counter/Counter';
 
 const initialState = {
   counter: 0,
+  results: new Array
 };
 
 const reducer = (state = initialState, action:any) => {
@@ -11,14 +12,16 @@ console.log(action.type);
   switch (action.type) {
       
     case 'INCREMENT':
-      return {
-        ...state,
-        counter: state.counter + 1,
-      };
+      //WAY1: it will give copy of state, but its not a deep clone
+      const newState = Object.assign({},state);
+      newState.counter = state.counter +1;
+      return newState;
     case 'DECREMENT':
       return {
+        //WAY2: spread operator taking out all values in state, and add the additional properties
         ...state,
         counter: state.counter - 1,
+        //If property already exist in object, it will override the value
       };
     case 'ADD':
       return {
@@ -30,6 +33,21 @@ console.log(action.type);
         ...state,
         counter: state.counter - action.val,
       };
+    case 'STORE_RESULT':
+      const resultFinal = Object.assign({},state);
+      resultFinal.results = state.results.concat({id: state.counter, value:state.counter});
+      return {
+        ...state,
+        results: resultFinal.results
+      }
+      
+      // return {
+      //   ...state,
+      //   results: state.results.concat({id: new Date(), value:state.counter})
+      // }
+      
+    case 'DELETE_RESULT':
+      return state;  
     default:
         return state
   }
--- src/containers/Counter/Counter.tsx
+++ src/containers/Counter/Counter.tsx
@@ -6,10 +6,13 @@ import CounterOutput from '../../components/Redux-Components/CounterOutput/Count
 
 interface ICounterProps{
     ctr:any,
+    storedResults:any
    onIncrementCounter: any,
    onDecrementCounter:any,
    onAddCounter:any,
-   onSubtractCounter:any
+   onSubtractCounter:any,
+   onStoreResult:any,
+   //onDeleteResult:any
 }
 
 
@@ -18,19 +21,29 @@ class Counter extends Component<ICounterProps,{}> {
         return (
             <div>
                 <CounterOutput value={this.props.ctr} />
-                <CounterControl label="Increment" clicked={() => this.props.onIncrementCounter()} />
-                <CounterControl label="Decrement" clicked={() => this.props.onDecrementCounter()}  />
-                <CounterControl label="Add 5" clicked={() => this.props.onAddCounter()}  />
-                <CounterControl label="Subtract 5" clicked={() => this.props.onSubtractCounter()}  />
-                
+                <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
+                <CounterControl label="Add 10" clicked={this.props.onAddCounter}  />
+                <CounterControl label="Subtract 10" clicked={this.props.onSubtractCounter}  />
+                <hr />
+                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <ul>
+                    {this.props.storedResults.map((strResult:any) => {
+                     return <li key={strResult.id}>{strResult.value}</li>
+                    })}
+                    
+                </ul>
             </div>
         );
     }
 }
 
 const mapStateToProps = (state:any) =>{
+    
+    console.log(state);
     return {
-        ctr: state.counter
+        ctr: state.counter,
+        storedResults: state.results,
     };
 }; 
 const mapDispatchToProps = (dispatch:any) =>{
@@ -41,7 +54,9 @@ const mapDispatchToProps = (dispatch:any) =>{
             dispatch({type:'INCREMENT'})},
         onDecrementCounter: ()=> dispatch({type:'DECREMENT'}),
         onAddCounter: ()=> dispatch({type:'ADD', val:10}),
-        onSubtractCounter: ()=> dispatch({type:'SUBTRACT', val:15})
+        onSubtractCounter: ()=> dispatch({type:'SUBTRACT', val:10}),
+        onStoreResult: ()=> dispatch({type:'STROE_RESULT'}),
+        OnDeleteResult: ()=> dispatch({type:'DELETE_RESULT'})
     };
 };
 
